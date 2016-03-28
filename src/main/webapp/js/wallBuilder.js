593c3b5f51ab49821d70edd951bef36d1bde87fe
Joshua Jolley <jolleyboy@gmail.com>
2016-03-28 14:28:09 -0600

    more project cleanup, almost ready for submission now

---
+++ src/main/webapp/js/wallBuilder.js
@@ -0,0 +1,67 @@
+// global wall variables
+var wallWidthRatio; // The width of the labyrinth walls
+var xLen; // length of a square
+var zLen; // height of a square
+var yLen; // height of the wall
+var wallMaterial;     // texture used for the labyrinth walls
+var goodMaze;
+var minimapWalls = new THREE.Object3D();
+function buildSimpleLaby(goodMaze)
+{	
+	Labyrinth = new THREE.Object3D();
+	var zLocation = -zDim * zLen / 2;
+	var cubeGeometry = new THREE.CubeGeometry(xLen, yLen, zLen);
+	var collisionGeometry = new THREE.CubeGeometry(xLen + 20, yLen + 20, zLen + 20);
+	for (var zCoord = 0; zCoord < zDim; zCoord++) {
+		var xLocation = -xDim * xLen / 2;
+		for (var xCoord = 0; xCoord < xDim; xCoord++) {
+			var wallMesh;
+			switch (goodMaze[zCoord][xCoord]) {
+				case 'X':
+					wallMesh = new THREE.Mesh(cubeGeometry, wallMaterial);
+					wallMesh.position.x = xLocation;
+					wallMesh.position.y = yLen / 2
+					wallMesh.position.z = zLocation;
+					wallMesh.castShadow = true;
+					wallMesh.recieveShadow = true;
+					var collisionMesh = new THREE.Mesh(collisionGeometry, collisionMaterial);
+					collisionMesh.position.x = xLocation;
+					collisionMesh.position.y = yLen / 2
+					collisionMesh.position.z = zLocation;
+					objects.push(collisionMesh);
+					minimapWalls.add(wallMesh.clone());
+					Labyrinth.add(wallMesh);
+					Labyrinth.add(collisionMesh);
+					break;
+				case '.':
+				case '/':
+			}
+
+			xLocation += xLen
+		}
+		zLocation += zLen
+	}
+	scene.add(Labyrinth);
+}
+
+function initWallVars(size)
+{
+	xLen = zLen = 40;    // the length/width of a square
+	yLen = 40;           // wall height
+	wallWidthRatio = 1; // Must be between 0 and 1. Change this to 1 to create "cube" walls.
+	xDim = size + 2;
+	zDim = size + 2;
+
+	// TODO: Fix texture ratio for long walls
+
+
+	var DiffuseTexture = loader.load("textures/stoneWall1.png");
+
+	wallMaterial = new THREE.MeshPhongMaterial(
+			{
+				map: DiffuseTexture,
+			});
+    wallMaterial.side = THREE.DoubleSide;
+	collisionMaterial = new THREE.MeshBasicMaterial();
+	collisionMaterial.visible = false;
+}
---
+++ target/rest/js/collisions.js
@@ -0,0 +1,100 @@
+/**
+ * Modified from mrdoob / http://mrdoob.com/
+ */
+
+raycaster = new THREE.Raycaster(new THREE.Vector3(), new THREE.Vector3(0, -1, 0), 0, 10);
+forwardRaycaster = new THREE.Raycaster(new THREE.Vector3(), new THREE.Vector3(1, 0, 0));
+backwardRaycaster = new THREE.Raycaster(new THREE.Vector3(), new THREE.Vector3(-1, 0, 0));
+rightRaycaster = new THREE.Raycaster(new THREE.Vector3(), new THREE.Vector3(0, 0, -1));
+leftRaycaster = new THREE.Raycaster(new THREE.Vector3(), new THREE.Vector3(0, 0, -1));
+
+function checkForCollisions() {
+
+	raycaster.ray.origin.copy(controls.getObject().position);
+	raycaster.ray.origin.y -= 10;
+
+	var cameraPosition = controls.getObject().position;
+	var cameraDirection = controls.getObject().getWorldDirection();
+
+	var forwardVector = cameraDirection.negate();
+	forwardVector.setY(cameraPosition.y);
+	forwardRaycaster.set(cameraPosition, forwardVector.normalize());
+
+	var backwardVector = cameraDirection;
+	backwardVector.setY(cameraPosition.y);
+	backwardRaycaster.set(cameraPosition, backwardVector.normalize());
+
+	var leftVector = cameraDirection.applyAxisAngle(new THREE.Vector3(1, 0, 0), (3 * Math.PI / 2));
+	leftVector.setY(cameraPosition.y);
+	leftRaycaster.set(cameraPosition, leftVector.normalize());
+
+	var rightVector = cameraDirection.applyAxisAngle(new THREE.Vector3(1, 0, 0), Math.PI / 2);
+	rightVector.setY(cameraPosition.y);
+	rightRaycaster.set(cameraPosition, rightVector.normalize());
+
+	var intersections = raycaster.intersectObjects(objects);
+	var forwardIntersections = forwardRaycaster.intersectObjects(objects);
+	var backwardIntersections = backwardRaycaster.intersectObjects(objects);
+	var rightIntersections = rightRaycaster.intersectObjects(objects);
+	var leftIntersections = leftRaycaster.intersectObjects(objects);
+
+	var isOnObject = intersections.length > 0;
+
+	var objectForward = forwardIntersections.length > 0;
+	var objectBackward = backwardIntersections.length > 0;
+	var objectRight = rightIntersections.length > 0;
+	var objectLeft = leftIntersections.length > 0;
+
+	var time = performance.now();
+	var delta = (time - prevTime) / 1000;
+
+	velocity.x -= velocity.x * 10.0 * delta;
+	velocity.z -= velocity.z * 10.0 * delta;
+
+	velocity.y -= 9.8 * 50.0 * delta; // 100.0 = mass
+
+	if (moveForward)
+		velocity.z -= 400.0 * delta;
+	if (moveBackward)
+		velocity.z += 400.0 * delta;
+
+	if (moveLeft)
+		velocity.x -= 400.0 * delta;
+	if (moveRight)
+		velocity.x += 400.0 * delta;
+
+	if (isOnObject === true) {
+		velocity.y = Math.max(0, velocity.y);
+
+		canJump = true;
+	}
+
+	if (objectLeft) {
+		velocity.x = Math.max(0, velocity.x);
+	}
+
+	if (objectForward) {
+		velocity.z = Math.max(0, velocity.z);
+	}
+
+	if (objectBackward) {
+		velocity.z = Math.min(0, velocity.z);
+	}
+
+	if (objectRight) {
+		velocity.x = Math.min(0, velocity.x);
+	}
+
+	controls.getObject().translateX(velocity.x * delta);
+	controls.getObject().translateY(velocity.y * delta);
+	controls.getObject().translateZ(velocity.z * delta);
+
+	if (controls.getObject().position.y < 10) {
+
+		velocity.y = 0;
+		controls.getObject().position.y = 10;
+
+		canJump = true;
+	}
+	prevTime = time;
+}
